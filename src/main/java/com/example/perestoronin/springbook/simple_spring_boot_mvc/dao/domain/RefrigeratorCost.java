d6661522a9aa507d23a46ed0ea47fb1742f3db29
Matvey Perestoronin <matveychick2004@yandex.ru>
2025-12-09 23:46:42 +0300

    refregirator

---
+++ src/main/java/com/example/perestoronin/springbook/simple_spring_boot_mvc/dao/domain/RefrigeratorCost.java
@@ -0,0 +1,13 @@
+package com.example.perestoronin.springbook.simple_spring_boot_mvc.dao.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="refrigerator_cost")
+public class RefrigetatorCost {
+    @Id
+    
+}
